988befad69aa11ea40b5b8c896327e2a3829af44
Felix Nithammer <f.nithammer@mammutmedia.eu>
2022-03-30 16:56:29 +0200

    BREAKING: first release

--- release.config.js
+++ release.config.js
@@ -36,15 +36,7 @@ async function config() {
 
   const config = {
     branches: [
-      "master",
-      // { name: 'alpha', prerelease: true },
-      // { name: 'beta', prerelease: true },
-      // 'next-major',
-      // Long-Term-Support branches
-      // { name: 'release-1', range: '1.x.x', channel: '1.x' },
-      // { name: 'release-2', range: '2.x.x', channel: '2.x' },
-      // { name: 'release-3', range: '3.x.x', channel: '3.x' },
-      // { name: 'release-4', range: '4.x.x', channel: '4.x' },
+      "master"
     ],
     dryRun: false,
     debug: true,
